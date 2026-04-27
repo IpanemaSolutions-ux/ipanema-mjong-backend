5a68b84daf5cf7726425913296c02e5d0248c45d
IpanemaSolutions-ux <wescosta30@gmail.com>
2026-04-27 20:19:22 +0100

    Update chat.ts

--- api/chat.ts
+++ api/chat.ts
@@ -1,6 +1,6 @@
 import { processChatMessage } from "../chatEngine.js";
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS liberado
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
